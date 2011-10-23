1dc34628b54f3f1fbee855d6d0ee2f7ee19da49c
Varun NotiBala <nbvarun@gmail.com>
2011-10-23 16:37:58 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
@@ -6,6 +8,8 @@ SampleApp::Application.routes.draw do
   get "pages/about"
   
   get "pages/help"
+
+  match '/signup', :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
